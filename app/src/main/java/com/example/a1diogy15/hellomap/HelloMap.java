07be871998a57f469f45f1462376eeb42badb947
1diogy15 <yora.diogo135@gmail.com>
2017-02-02 12:49:26 +0000

    Change default location from Fernhurst to Litochoro

--- app/src/main/java/com/example/a1diogy15/hellomap/HelloMap.java
+++ app/src/main/java/com/example/a1diogy15/hellomap/HelloMap.java
@@ -3,12 +3,16 @@ package com.example.a1diogy15.hellomap;
 import android.app.Activity;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import org.osmdroid.views.MapView;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.config.Configuration;
 
-public class HelloMap extends Activity
-{
+public class HelloMap extends Activity implements View.OnClickListener {
 
     MapView mv;
 
@@ -28,5 +32,15 @@ public class HelloMap extends Activity
         mv.setBuiltInZoomControls(true);
         mv.getController().setZoom(14); //HIGHER numbers = more zoom in
         mv.getController().setCenter(new GeoPoint(40.1,22.5));
+
+        Button button = (Button)findViewById(R.id.set_position);
+        button.setOnClickListener(this);
+    }
+
+    public void onClick(View view)
+    {
+        EditText latitude = (EditText)findViewById(R.id.latitude);
+        EditText longitude = (EditText)findViewById(R.id.longitude);
+
     }
 }
