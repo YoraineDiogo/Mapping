24f8bb73f17abb4c70e9793ac77977f8ee2d9b70
1diogy15 <yora.diogo135@gmail.com>
2017-03-09 11:52:07 +0000

    menus and multiple activities

--- app/src/main/java/com/example/a1diogy15/hellomap/HelloMap.java
+++ app/src/main/java/com/example/a1diogy15/hellomap/HelloMap.java
@@ -1,13 +1,18 @@
 package com.example.a1diogy15.hellomap;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.views.MapView;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.config.Configuration;
@@ -16,8 +21,7 @@ public class HelloMap extends Activity implements View.OnClickListener {
 
     MapView mv;
 
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -27,20 +31,60 @@ public class HelloMap extends Activity implements View.OnClickListener {
         Configuration.getInstance().load
                 (this, PreferenceManager.getDefaultSharedPreferences(this));
 
-        mv = (MapView)findViewById(R.id.map1);
+        mv = (MapView) findViewById(R.id.map1);
 
         mv.setBuiltInZoomControls(true);
         mv.getController().setZoom(14); //HIGHER numbers = more zoom in
-        mv.getController().setCenter(new GeoPoint(40.1,22.5));
+        mv.getController().setCenter(new GeoPoint(40.1, 22.5));
 
-        Button button = (Button)findViewById(R.id.set_position);
+        Button button = (Button) findViewById(R.id.set_position);
         button.setOnClickListener(this);
     }
 
-    public void onClick(View view)
-    {
-        EditText latitude = (EditText)findViewById(R.id.latitude);
-        EditText longitude = (EditText)findViewById(R.id.longitude);
+    public void onClick(View view) {
+        EditText latitude = (EditText) findViewById(R.id.latitude);
+        EditText longitude = (EditText) findViewById(R.id.longitude);
 
     }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_hello_map, menu);
+        return true;
+    }
+
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.choosemap) {
+            // react to the menu item being selected...
+            Intent intent = new Intent(this, MapChooseActivity.class);
+            startActivityForResult(intent,0);
+            return true;
+        }
+        return false;
+    }
+
+
+    protected void onActivityResult(int requestCode,int resultCode,Intent intent)
+    {
+
+        if(requestCode==0)
+        {
+
+            if (resultCode==RESULT_OK)
+            {
+                Bundle extras=intent.getExtras();
+                boolean cyclemap = extras.getBoolean("com.example.cyclemap");
+                if(cyclemap==true)
+                {
+                    mv.setTileSource(TileSourceFactory.CYCLEMAP);
+                }
+                else
+                {
+                    mv.setTileSource(TileSourceFactory.MAPNIK);
+                }
+            }
+        }
+    }
+
 }
